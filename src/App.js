16f14fef3fd9c98b2e927ef959d6a9d184384b5e
oloomoses <olooine@gmail.com>
2021-02-01 01:28:19 +0300

    add users search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from "./components/users/Users";
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -20,13 +21,23 @@ class App extends Component {
     this.setState({users: res.data, loading: false });
   }
   
+  searUsers = async (text) => {
+    const client_id = process.env.REACT_APP_GH_CLIENT_ID;
+    const client_secret = process.env.REACT_APP_GH_CLIENT_SECRET;
+    const url = `https://api.github.com/search/users?q=${text}&client_id=${client_id}&client_secret=${client_secret}`
+
+    this.setState({ loading: true })
+    const res = await axios.get(url);
+    
+    this.setState({users: res.data.items, loading: false });
+  }
   
   render() {
       return (
       <div className="App">
         <Navbar />
         <div className = "container">
-
+          <Search searchUsers = { this.searUsers }/>
           <Users loading = { this.state.loading } users = { this.state.users } />
         </div>
         
