b86834e1eee1d5c04c1810a64c268d04dd5e1215
oloomoses <olooine@gmail.com>
2021-01-31 23:55:32 +0300

    refactor to function based components fetch and display users from github api add spinner component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from "./components/users/Users";
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({ loading: true })
+    const res = await axios.get('https://api.github.com/users');
+    
+    this.setState({users: res.data, loading: false });
+  }
   
   
   render() {
@@ -13,7 +23,7 @@ class App extends Component {
         <Navbar />
         <div className = "container">
 
-          <Users />
+          <Users loading = { this.state.loading } users = { this.state.users } />
         </div>
         
       </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: {login, avatar_url, html_url} }) => {
     return (
       <div className = "card text-center">
         <img src = { avatar_url } 
@@ -18,7 +16,10 @@ class UserItem extends Component {
       </div>
       
     )
-  }
+}
+
+UserItem.propType = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
